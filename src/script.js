0ace13039da559f05e361159ce19e7d09281a894
roaris <one0803kyopro@gmail.com>
2021-09-20 00:07:07 +0900

    first

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.getElementById('alert').addEventListener('click', function (){
+  alert("document.getElementByIdのalert");
+});;
+
+$('#alert').on('click', function () {
+  console.log('jQueryのalert');
+  alert('jQueryのalert');
+});
